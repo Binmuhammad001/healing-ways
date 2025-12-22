f3ffffda76ffbb284f5270fff3c3c1c162ba5035
Nabil muhammad <102354610+Binmuhammad001@users.noreply.github.com>
2025-12-22 17:32:37 +0100

    Update section3.jsx

--- src/components/home/section3.jsx
+++ src/components/home/section3.jsx
@@ -33,7 +33,7 @@ export default function Section3() {
             </h2>
 
             <p className="text-gray-700 text-sm sm:text-base mb-8">
-              We are committed to making your medical tourism journey a success,
+              We are committed to making your medical tourism journey a success
               <br />
               from initial consultation to post-treatment follow-up.
             </p>
