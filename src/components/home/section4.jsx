2bb4187f00a9b2470f9d47ad0599dcfa57a70360
Nabil muhammad <102354610+Binmuhammad001@users.noreply.github.com>
2025-12-22 14:35:08 +0100

    Update section4.jsx

--- src/components/home/section4.jsx
+++ src/components/home/section4.jsx
@@ -21,8 +21,10 @@ export default function Section4() {
               Get access to medical consultants anytime
             </h2>
 
-            <p className="text-gray-700 text-sm sm:text-base mb-8">
-              We are committed to making your medical tourism journey a success, <br />from initial consultation to post-treatment follow-up.
+             <p className="text-gray-700 text-sm sm:text-base mb-8">
+              We are committed to making your medical tourism journey a success,
+              <br />
+              from initial consultation to post-treatment follow-up.
             </p>
 
             {/* Benefits List */}
