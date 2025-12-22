f53110eaf6fb7f3a51ad4c22348355d44fc914dc
Nabil muhammad <102354610+Binmuhammad001@users.noreply.github.com>
2025-12-22 13:30:02 +0100

    Update hero.jsx

--- src/components/home/hero.jsx
+++ src/components/home/hero.jsx
@@ -17,7 +17,7 @@ export default function Hero() {
 </p>
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-gray-900">
   Your compass to <br />
-  <span className="pt-6 inline-block">healthy living</span>
+  <span className="pt-8 inline-block">healthy living</span>
 </h1>
 
 
