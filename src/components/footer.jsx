33fa6bcaa7dbf80ec70b59f0f321446897d0138d
Nabil muhammad <102354610+Binmuhammad001@users.noreply.github.com>
2025-12-22 23:03:19 +0100

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ import Logo from "../assets/logo.svg";
 export default function Footer() {
   return (
     <footer className="bg-[#172641] text-white py-12 px-4 w-full">
-      <div className="w-full px-4">
+      <div className="w-full px-8">
         {/* Main Footer Content */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mb-8  m-8">
           
