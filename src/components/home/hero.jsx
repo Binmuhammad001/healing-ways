0bcb3a430bded1e0d9eac8cf55487c45e0ae3df0
Nabil muhammad <102354610+Binmuhammad001@users.noreply.github.com>
2025-12-22 13:19:16 +0100

    Update hero.jsx

--- src/components/home/hero.jsx
+++ src/components/home/hero.jsx
@@ -15,9 +15,10 @@ export default function Hero() {
            <p className="font-semibold uppercase mb-2" style={{ color: '#137EE8' }}>
   Medical Consulting Services
 </p>
-            <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-gray-900">
-              Your compass to <br />healthy living
-            </h1>
+            <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-gray-900 leading-snug">
+  Your compass to <br /> healthy living
+</h1>
+
           </div>
 
           {/* Right Column */}
