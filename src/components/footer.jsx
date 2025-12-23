c253dd5361146f3fed4d2f11172ba7569f48f070
Nabil muhammad <102354610+Binmuhammad001@users.noreply.github.com>
2025-12-23 11:29:17 +0100

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -133,7 +133,7 @@ export default function Footer() {
 
             {/* Copyright */}
             <p className="text-gray-400 text-sm">
-              © 2025 • All rights reserved
+              © 2025  All rights reserved
             </p>
           </div>
         </div>
