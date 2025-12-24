8905c7fb96511779a7f6c50990328d47fbce2d83
Nabil muhammad <102354610+Binmuhammad001@users.noreply.github.com>
2025-12-24 12:09:49 +0100

    Update contact.jsx

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
 import Contact from '../components/contact/contact';
+import Location from '../components/contact/location';
 
 export default function App(){
     return(
        <>
        <Navbar/>
        <Contact/>
+       <Location/>
        <Footer/>
        </> 
     )
