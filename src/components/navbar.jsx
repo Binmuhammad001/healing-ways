83e12e82f6db17a4c3da170c582e5831d66b84db
Nabil muhammad <102354610+Binmuhammad001@users.noreply.github.com>
2026-01-01 17:17:17 +0100

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -118,6 +118,19 @@ export default function Navbar() {
               Contact
             </a>
           </li>
+
+           <li className="md:hidden">
+            <a href="/login" className="block py-2 px-4 text-gray-700 hover:text-blue-600">
+              Login
+            </a>
+          </li>
+
+          <li className="md:hidden">
+            <a href="/signup" className="block py-2 px-4 text-gray-700 hover:text-blue-600">
+              Book Appointment
+            </a>
+          </li>
+          
         </ul>
 
         {/* RIGHT BUTTONS */}
