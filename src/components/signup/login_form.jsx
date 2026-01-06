d8259930b908f77798568b85dc0e80865145dcc6
Nabil muhammad <102354610+Binmuhammad001@users.noreply.github.com>
2026-01-06 13:24:48 +0100

    Update login_form.jsx

--- src/components/signup/login_form.jsx
+++ src/components/signup/login_form.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from "../../context/AuthContext";
+
 
 export default function LoginForm() {
   const navigate = useNavigate();
