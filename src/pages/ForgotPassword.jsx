e02504c483b7715181d9136271c7549054ce1469
Nabil muhammad <102354610+Binmuhammad001@users.noreply.github.com>
2026-01-08 21:46:50 +0100

    Create ForgotPassword.jsx

---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../components/navbar';
+import Footer from '../components/footer';
+import ForgotPassword from '../components/signup/ForgotPassword';
+
+export default function App(){
+
+  return(
+    <>
+      <Navbar/>
+      <ForgotPassword/>
+      <Footer/>
+    </>
+  )  
+  
+}
+  
