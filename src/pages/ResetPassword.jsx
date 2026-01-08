5ebf91f1ead80d242584c6865e32ea41439a1065
Nabil muhammad <102354610+Binmuhammad001@users.noreply.github.com>
2026-01-08 21:51:18 +0100

    Create ResetPassword.jsx

---
+++ src/pages/ResetPassword.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../components/navbar';
+import ResetPassword from '../components/signup/ResetPassword';
+import Footer from '../components/footer';
+
+
+export default function App (){
+  return (
+    <>
+      <Navbar/>
+      <ResetPassword/>
+      <Footer/>
+    </>
+  )
+}
