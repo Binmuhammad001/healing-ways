fa657ea26cdc574c40ebc1526ab168ab10e7c3e0
Nabil muhammad <102354610+Binmuhammad001@users.noreply.github.com>
2026-01-09 22:44:58 +0100

    Update contact.jsx

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -71,7 +71,7 @@ export default function ContactSalesForm() {
   };
 
   return (
-    <section className="bg-white py-16 md:py-20 pt-32">
+    <section className="bg-white py-16 md:py-20 pt-40">
       <div className="container mx-auto px-6 max-w-7xl">
         <div className="grid md:grid-cols-2 gap-12 items-center">
           {/* Left side - Form */}
