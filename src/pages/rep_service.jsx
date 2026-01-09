df09377eb8d7f95df5fa8963f11c9935d3e6a400
Nabil muhammad <102354610+Binmuhammad001@users.noreply.github.com>
2026-01-09 21:05:16 +0100

    Update rep_service.jsx

--- src/pages/rep_service.jsx
+++ src/pages/rep_service.jsx
@@ -14,10 +14,6 @@ export default function App(){
         <>
         <Navbar/>
         <Hero/>
-        <Section1/> 
-        <Section2/>
-        <Section3/>
-        <Section4/>
         <Footer/>
         </>
     )
