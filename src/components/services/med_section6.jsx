c0315dd7769a36c17eb01e2d25ec9d38bbed09fc
Nabil muhammad <102354610+Binmuhammad001@users.noreply.github.com>
2026-01-12 13:06:58 +0100

    Update med_section6.jsx

--- src/components/services/med_section6.jsx
+++ src/components/services/med_section6.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Minor from '../../assets/minor.jpg';
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../context/AuthCotext";
+import { useAuth } from "../../context/AuthContext";
 
 export default function Section8() {
    const navigate = useNavigate();
