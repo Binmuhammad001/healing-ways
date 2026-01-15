c84fdf6d2093767cf940be83fc8fa10eb8663c03
Nabil muhammad <102354610+Binmuhammad001@users.noreply.github.com>
2026-01-15 10:58:37 +0100

    Update rep_hero.jsx

--- src/components/services/rep_hero.jsx
+++ src/components/services/rep_hero.jsx
@@ -40,7 +40,8 @@ export default function HeroSection() {
           </h1>
           
           <p className="text-sm sm:text-base md:text-lg text-gray-100 leading-relaxed max-w-xl mx-auto md:mx-0">
-            Connecting people or medically challenged individuals to the right hospitals both locally and internationally to receive the best possible care.
+           Connecting patients to the right hospitals, and doctors worldwide to receive the best possible care geared towards restoring health and hope ,
+           To be reborn and thrive even better 
           </p>
           
           <div className="pt-2 flex justify-center md:justify-start">
